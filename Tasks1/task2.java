fc4ca5840810e50df34c1c28f996dac383716e73
Sergey <hardblock01@gmail.com>
2023-02-17 16:45:53 +0300

    Complete task2

---
+++ Tasks1/task2.java
@@ -0,0 +1,28 @@
+//2. Вывести все простые числа от 1 до 1000
+
+package Java_Tasks.Tasks1;
+
+public class task2 {
+    public static void main(String[] args) {
+        onethousandsimpledigits(1000, 1000);
+    }
+    public static void onethousandsimpledigits(int x, int y) {
+        int counter = 0;
+        while (counter < y) {
+            if (simple(x)) {
+                System.out.println("i = " + x);
+                counter++;
+            }
+            x++;
+        }
+    }
+
+    public static boolean simple(int num) {
+        for (int x = 2; x < num; x++) {
+            if (num % x == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
